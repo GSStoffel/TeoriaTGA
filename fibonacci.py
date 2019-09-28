166361259e7062d5415c989dc533a9327d9037b5
feliperflores <feliperflores2@hotmail.com>
2019-09-28 17:06:27 -0300

    Refactor

--- fibonacci.py
+++ fibonacci.py
@@ -1,27 +1,20 @@
-import sys
 import os
+import sys
 
-def fibonacci_encode(numero):
-    saida = ""
-    if numero >= 1:
-        pri_aux = 1
-        seg_aux = 1
-        ter_aux = pri_aux + seg_aux
-        fibonacci = [seg_aux]
+def leitor(numero):
+    global array_de_bytes
+    global posicao_bit
+    str_bit = ''
 
-        while numero >= ter_aux:
-            fibonacci.append(ter_aux)
-            pri_aux = seg_aux
-            seg_aux = ter_aux
-            ter_aux = pri_aux + seg_aux
-        saida = "1"
-        for numero_fibonacci in reversed(fibonacci):
-            if numero >= numero_fibonacci:
-                numero = numero - numero_fibonacci
-                saida = "1" + saida
-            else:
-                saida = "0" + saida
-    return saida
+    for i in range(numero):
+        posicao_bit_no_byte = 7 - (posicao_bit % 8)
+        posicao_byte = int(posicao_bit / 8)
+        byte_valor = array_de_bytes[posicao_byte]
+        valor_bit = int(byte_valor / (2 ** posicao_bit_no_byte)) % 2
+        str_bit += str(valor_bit)
+        posicao_bit += 1
+
+    return str_bit
 
 def escrita(str_bit, nome_arquivo_saida):
     global bit_stream
@@ -33,21 +26,32 @@ def escrita(str_bit, nome_arquivo_saida):
         chr_int_str_byte = chr(int(str_byte, 2))
         nome_arquivo_saida.write(bytes(chr_int_str_byte))
 
-def leitor(n):
-    global array_de_bytes
-    global posicao_bit
-    str_bit = ''
-    for i in range(n):
-        bitPosInByte = 7 - (posicao_bit % 8)
-        bytePosition = int(posicao_bit / 8)
-        byte_valor = array_de_bytes[bytePosition]
-        bitVal = int(byte_valor / (2 ** bitPosInByte)) % 2
-        str_bit += str(bitVal)
-        posicao_bit += 1
-    return str_bit
+def fibonacci_encode(numero):
+    saida = ""
+    if numero >= 1:
+        pri_aux = 1
+        seg_aux = 1
+        prox_num = pri_aux + seg_aux
+        lista_numeros = [seg_aux]
+        saida = "1"
+
+        while numero >= prox_num:
+            lista_numeros.append(prox_num)
+            pri_aux = seg_aux
+            seg_aux = prox_num
+            prox_num = pri_aux + seg_aux
+
+        for numero_fibonacci in reversed(lista_numeros):
+            if numero >= numero_fibonacci:
+                numero = numero - numero_fibonacci
+                saida = "1" + saida
+            else:
+                saida = "0" + saida
+
+    return saida
 
 if len(sys.argv) != 4:
-    print('Uso: python fibonacci.py [encode / decode] nomeArquivoEntrada nomeArquivoSaida')
+    print('Para rodar o programa: python fibonacci.py [encode / decode] nomeArquivoEntrada nomeArquivoSaida')
     sys.exit()
 
 entrada = sys.argv[1]
@@ -61,14 +65,16 @@ arquivo_input.close()
 tamanho_arquivo = len(array_de_bytes)
 
 if entrada == 'encode':
-    freqList = [0] * 256
+    lista_frequencia = [0] * 256
+
     for b in array_de_bytes:
-        freqList[b] += 1
+        lista_frequencia[b] += 1
 
     lista = []
+
     for b in range(256):
-        if freqList[b] > 0:
-            lista.append((freqList[b], b, ''))
+        if lista_frequencia[b] > 0:
+            lista.append((lista_frequencia[b], b, ''))
 
     lista = sorted(lista, key=lambda tupla: tupla[0], reverse = True)
 
@@ -102,6 +108,7 @@ elif entrada == 'decode':
     posicao_bit = 0
     n = int(leitor(8), 2) + 1
     dic = dict()
+
     for i in range(n):
         valor_do_byte = int(leitor(8), 2)
         bit_str_encoding = fibonacci_encode(i + 1)
